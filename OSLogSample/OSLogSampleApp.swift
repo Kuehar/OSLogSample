fb764ef081c3868278bd556069e196f219c4a038
kuehar <green95251@gmail.com>
2023-10-15 15:54:10 +0900

    Initial Commit

---
+++ OSLogSample/OSLogSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSLogSampleApp.swift
+//  OSLogSample
+//
+//  Created by kuehar on 2023/10/15.
+//
+
+import SwiftUI
+
+@main
+struct OSLogSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
